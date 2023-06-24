9bc4dc056c3ec4b0200c6388a3148873239094dc
Yaşar ÖKTEN <yasaroktnn@gmail.com>
2023-06-24 23:51:32 +0300

    hata kontrolleri yapıldı.

--- 2-WEEK/Z95636.CBL(DAYCALC).cbl
+++ 2-WEEK/Z95636.CBL(DAYCALC).cbl
@@ -1,17 +1,35 @@
       *Bu bölüm programın kimlik bilgilerinin belirtildiği bölümdür.
+      *Bu bölümde programın adını ve programı oluşturan kişi
+      *belirtilmiştir.
        IDENTIFICATION DIVISION.
        PROGRAM-ID. DAYCALC.
        AUTHOR. Yasar Okten.
+      *Bu bölüm programın çalışma ortamını ve kullanılacak dosyaların
+      *tanımlarını içeren önemli bir bölümdür.
        ENVIRONMENT DIVISION.
+      *Bu bölümde programın kullanacağı girdi ve çıktı dosyaları
+      *tanımlanmaktadır.
        INPUT-OUTPUT SECTION.
+      *Bu kod bloğu, programın "PRT-LINE" ve "DATE-REC" dosyalarını
+      *kullanacağını ve bu dosyaların fiziksel bağlantılarını ve 
+      *durumlarını belirlediğini göstermektedir.
+      *Bu tanımlamalar, programın dosya işlemlerini yapabilmesi için
+      * gerekli olan dosya bağlantılarını ve durum kontrollerini sağlar.
        FILE-CONTROL.
            SELECT PRT-LINE ASSIGN PRTLINE
                                   STATUS ST-PRINT-LINE.
            SELECT DATE-REC ASSIGN DATEREC
                                   STATUS ST-DATE-REC.
+      *Bu bölüm programın veri yapılarını organize etmesini ve veri
+      *erişimi ve işleme işlevlerini gerçekleştirmesini sağlar. 
+      *Bu bölümde tanımlanan veriler, programın çalışması sırasında
+      *veri alışverişi, veri depolama ve işlem yapma yeteneklerini
+      *sağlar.
        DATA DIVISION.
        FILE SECTION.
        FD  PRT-LINE RECORDING MODE F.
+      *"PRT-LINE" adlı dosyanın tanımının başladığını belirtir ve kayıt
+      *yapısının "Fixed" (F) modda olduğunu ifade eder. 
          01  RECORD-PRT-LINE.
            05 PRT-LINE-SRT      PIC 9(4).
            05 PRT-LINE-NAME     PIC A(15).
@@ -20,6 +38,9 @@
            05 PRT-LINE-TODAY    PIC 9(8).
            05 PRT-LINE-TDAY     PIC 9(5).
        FD  DATE-REC RECORDING MODE F.
+      *"FD DATE-REC RECORDING MODE F." ifadesi, "DATE-REC" adlı dosyanın
+      *tanımının başladığını belirtir ve kayıt yapısının "Fixed" (F) 
+      *modda olduğunu ifade eder.
          01   RECORD-DATE-REC.
            05 DATE-REC-SRT      PIC 9(4).
            05 DATE-REC-NAME     PIC A(15).
@@ -37,17 +58,24 @@
              88 ST-DATE-SC         VALUE 00 97.
              88 ST-DATE-EOF        VALUE 10.
        PROCEDURE DIVISION.
+      *Maini oluşturduk burada yazılı olan sırada paragrafları takip
+      *edip programı tamamlayacağız.
        0000-MAIN.
            PERFORM H100-OPEN-FILE.
            PERFORM H200-PROCESS UNTIL ST-DATE-EOF.
            PERFORM H999-CLOSE-FILE.
        0000-END. EXIT.
+      *INPUT ve OUTPUT dosyalarını açtık ve output dosyasının ilk
+      *satırını okuduk.
        H100-OPEN-FILE.
            OPEN INPUT  DATE-REC.
            OPEN OUTPUT PRT-LINE.
            PERFORM H110-OPEN-FILE-CONTROL.
            READ DATE-REC.
        H100-END. EXIT.
+      *Dosyalar açılırken bir problemle karşılaşıldı mı diye kontrol
+      *ettik eğer ki karşılaşılmışsa hata mesajı yazdırıp programı 
+      *sonlandırdık.
        H110-OPEN-FILE-CONTROL.
            IF (ST-DATE-REC NOT = 97) AND (ST-DATE-REC NOT = 0)
             DISPLAY 'OUTPUT FILE NOT OPEN.'
@@ -58,6 +86,10 @@
               PERFORM H999-CLOSE-FILE
            END-IF.
        H110-END. EXIT.
+      *INPUT dosyasından edindiğimiz bilgileri OUTPUT dosyamıza aktarmak
+      *için OUTPUT dosyası için oluştuduğumuz değişkenlere bu bilgileri
+      *yerleştiriyoruz ve yapılması gereken matematiksel işlemleri
+      *yapıyoruz.
        H200-PROCESS.
            COMPUTE BDAY-INT = FUNCTION INTEGER-OF-DATE(DATE-REC-BDAY).
            COMPUTE TODAY-INT = FUNCTION INTEGER-OF-DATE(DATE-REC-TODAY).
@@ -71,6 +103,7 @@
            WRITE RECORD-PRT-LINE.
            READ DATE-REC.
        H200-END. EXIT.
+      *INPUT ve OUTPUT dosyalarını kapatıp programı sonlandırıyoruz.
        H999-CLOSE-FILE.
            CLOSE DATE-REC.
            CLOSE PRT-LINE.
